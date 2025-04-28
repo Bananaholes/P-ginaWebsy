2cc749f35f528bcb95cd8b3dd90403f8f887b0b1
Bananaholmes <germanicocapo@gmail.com>
2025-04-27 21:56:50 -0300

    Comienzo de la página de Websy

---
+++ index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def principal():
+    return render_template("index.html")
+
+
+@app.route('/Contactos')
+def contacto():
+    return render_template("contacto.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5020)
